c4a0bff40cfb27cba913daf030cc31eca137bed5
Daniel Cook <danielecook@gmail.com>
2015-10-29 14:22:13 -0500

    use custom delimiter

--- md.py
+++ md.py
@@ -10,6 +10,7 @@ from tabulate import tabulate
 import csv
 from io import StringIO
 import re
+import sys
 
 __version__ = '0.2'
 
@@ -19,29 +20,36 @@ def main(wf):
     # you call from `Workflow.run`
 
     # Get args from Workflow as normalized Unicode
-    args = wf.args
-
+    delim = wf.args
     cb = pyperclip.paste().strip()
 
-    try:
-        dialect = csv.Sniffer().sniff(StringIO(cb).read(1024))
-        cb = list(csv.reader(StringIO(unicode(cb), newline=None), dialect))
-    except:
-        cb = [x.split("\t")  for x in re.sub(" +","\t", cb).split("\n")]
+    custom = ""
+    if len(delim[0]) == 0:
+        try:
+            dialect = csv.Sniffer().sniff(StringIO(cb).read(1024))
+            cb = list(csv.reader(StringIO(unicode(cb), newline=None), dialect))
+        except:
+            cb = [x.decode('utf-8').split(delim)  for x in re.sub(" +","\t", cb).split("\n")]
+    else:
+        # Custom delimiter
+        custom = " - Custom ==> " + str(delim[0])
+        cb = [re.split(delim[0], x) for x in cb.decode("utf-8").split("\n")]
+        log.debug(cb)
 
     try:
         # Header
         cb_grid_header = tabulate(cb,  headers="firstrow", tablefmt="pipe")
         cb_grid_header_prev = "L1: " + cb_grid_header.split("\n")[0] + ";L2: " + cb_grid_header.split("\n")[2]
-        wf.add_item("Header", cb_grid_header_prev, arg=cb_grid_header, valid=True)
+        wf.add_item("Header" + custom, cb_grid_header_prev, arg=cb_grid_header, valid=True)
       
         # No header
         cb_grid = tabulate(cb, tablefmt="pipe")
         cb_grid_prev = cb_grid.split("\n")[1]
 
-        wf.add_item("No Header", cb_grid_prev, arg=cb_grid, valid=True)
+        wf.add_item("No Header" + custom, cb_grid_prev, arg=cb_grid, valid=True)
     except:
         wf.add_item("Error", "Unable to parse table", icon = "error.png")
+
 
     
     # Send output to Alfred
